87505c8eb75e512c35fd16ab253e16c7778d3119
liyuan <liyuang@qihangjinfu.com>
2021-05-11 14:26:03 +0800

    origin提交2

--- src/main/java/com/example/liyuan/sorter/BranchTest.java
+++ src/main/java/com/example/liyuan/sorter/BranchTest.java
@@ -3,5 +3,6 @@ package com.example.liyuan.sorter;
 public class BranchTest {
     public static void main(String[] args) {
         System.out.println("耶耶耶耶耶！");
+        System.out.println("啦啦啦啦啦！");
     }
 }
