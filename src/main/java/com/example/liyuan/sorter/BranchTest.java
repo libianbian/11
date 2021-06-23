50e4fab1cd8ffb74a94bc115df81cef0dc1bc3ac
liyuan <liyuan@qihangjinfu.com>
2021-06-23 14:07:02 +0800

    origin提交4

--- src/main/java/com/example/liyuan/sorter/BranchTest.java
+++ src/main/java/com/example/liyuan/sorter/BranchTest.java
@@ -2,6 +2,6 @@ package com.example.liyuan.sorter;
 
 public class BranchTest {
     public static void main(String[] args) {
-        System.out.println("耶耶耶耶耶！");
+        System.out.println("oooooo！");
     }
 }
