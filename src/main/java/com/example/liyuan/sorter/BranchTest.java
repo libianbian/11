04e68160f5b126da1ae2d2fe1e6e6e16bb00082d
libianbian <40421452+libianbian@users.noreply.github.com>
2021-06-23 14:16:04 +0800

    Update BranchTest.java

--- src/main/java/com/example/liyuan/sorter/BranchTest.java
+++ src/main/java/com/example/liyuan/sorter/BranchTest.java
@@ -2,6 +2,7 @@ package com.example.liyuan.sorter;
 
 public class BranchTest {
     public static void main(String[] args) {
-        System.out.println("oooooo！");
+        System.out.println("零零零零！");
+        System.out.println("呀呀呀呀！");
     }
 }
